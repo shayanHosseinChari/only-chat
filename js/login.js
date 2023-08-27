7755e75776eaacbe1a770decb7f82ac0a888b6fa
Reza <i.am.reza.programmer@gmail.com>
2023-08-27 11:42:21 +0330

    Some change in login

--- js/login.js
+++ js/login.js
@@ -1,17 +1,5 @@
-const checkbox = document.querySelector("#checkbox");
-const formBtn = document.querySelector(".form-btn");
 const eyeIcon = document.querySelector(".eye-icon");
 const eyeIconSlash = document.querySelector(".eye-icon-slash");
-
-checkbox.addEventListener("input", () => {
-  if (checkbox.checked) {
-    formBtn.classList.remove("disabled");
-    formBtn.setAttribute("disabled", false);
-  } else {
-    formBtn.classList.add("disabled");
-    formBtn.setAttribute("disabled", true);
-  }
-});
 
 eyeIcon.addEventListener("click", (e) => {
   e.target.parentNode.children[0].classList.toggle("show");
